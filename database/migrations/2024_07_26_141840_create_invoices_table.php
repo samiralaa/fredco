65249756a574f286891be2375bd1ec28b097bf1b
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2024-07-26 18:57:56 +0300

    aaa

--- database/migrations/2024_07_26_141840_create_invoices_table.php
+++ database/migrations/2024_07_26_141840_create_invoices_table.php
@@ -9,19 +9,35 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->string('client_name');
+            $table->string('client_location');
+            $table->string('client_address');
+            $table->string('company_name');
+            $table->string('company_taxId');
+            $table->string('company_location');
+            $table->string('company_address');
+            $table->date('created_date');
+            $table->date('due_date');
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+        });
+
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->constrained()->onDelete('cascade');
+            $table->string('description');
+            $table->decimal('amount', 10, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
+        Schema::dropIfExists('invoice_items');
         Schema::dropIfExists('invoices');
     }
 };
